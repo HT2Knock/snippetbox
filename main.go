c282748b44e95894a3277ba6a000566efd06104e
T2Knock <huynhtransongngoc@gmail.com>
2025-12-05 16:17:04 +0700

    feat: add http listen and serve

--- main.go
+++ main.go
@@ -1,12 +1,42 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/T2Knock/snippetbox/hello"
+	"log"
+	"net/http"
 )
 
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	w.Write([]byte("Hello from snippet"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.Header().Add("Allowed", http.MethodPost)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Write([]byte("Create a new snippet..."))
+}
+
 func main() {
-	fmt.Println(hello.Hello())
-	fmt.Println(hello.Proverb())
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("GET /snippet/view", snippetView)
+	mux.HandleFunc("POST /snippet/create", snippetCreate)
+
+	log.Println("Starting server on :4000")
+
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
