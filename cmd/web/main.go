315aa2e7b97b992fdc2fae33ae8ed8e3708ba2ed
T2Knock <huynhtransongngoc@gmail.com>
2025-12-22 19:17:18 +0700

    feat: serving static asset

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,6 +8,9 @@ import (
 func main() {
 	mux := http.NewServeMux()
 
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("GET /snippet/view", snippetView)
 	mux.HandleFunc("POST /snippet/create", snippetCreate)
