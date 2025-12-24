4c12cd2b3cea2010b656a6c0da019f00d9966575
T2Knock <huynhtransongngoc@gmail.com>
2025-12-24 12:27:05 +0700

    refactor: setting cmd flag for config value

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,22 +1,34 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
+type config struct {
+	addr   string
+	static string
+}
+
 func main() {
+	var cfg config
+
+	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
+	flag.StringVar(&cfg.static, "static", "./ui/static/", "Path to static assets")
+
+	flag.Parse()
+
 	mux := http.NewServeMux()
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	fileServer := http.FileServer(http.Dir(cfg.addr))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("GET /snippet/view", snippetView)
 	mux.HandleFunc("POST /snippet/create", snippetCreate)
 
-	log.Println("Starting server on :4000")
-
-	err := http.ListenAndServe(":4000", mux)
+	log.Printf("Starting server on %s", cfg.addr)
+	err := http.ListenAndServe(cfg.addr, mux)
 	log.Fatal(err)
 }
