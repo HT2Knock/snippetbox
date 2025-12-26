bbce0f89abdac616d6ce134f7d0fb49ce0dd9914
T2Knock <huynhtransongngoc@gmail.com>
2025-12-26 18:05:48 +0700

    feat: open connection to mysql

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,21 +1,26 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type config struct {
 	addr   string
 	static string
+	dsn    string
 }
 
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
 	cfg      config
+	db       *sql.DB
 }
 
 func main() {
@@ -23,15 +28,29 @@ func main() {
 
 	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
 	flag.StringVar(&cfg.static, "static", "./ui/static/", "Path to static assets")
+	flag.StringVar(&cfg.dsn, "dsnc", "root:snippetbox@/snippetbox", "MySQL data source name")
 
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(cfg.dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer func() {
+		if err := db.Close(); err != nil {
+			errorLog.Printf("error closing the database %v", err)
+		}
+	}()
+
 	app := application{
 		infoLog:  infoLog,
 		errorLog: errorLog,
 		cfg:      cfg,
+		db:       db,
 	}
 
 	srv := &http.Server{
@@ -41,6 +60,18 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s", cfg.addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
--- go.mod
+++ go.mod
@@ -2,9 +2,6 @@ module github.com/T2Knock/snippetbox
 
 go 1.25.3
 
-require rsc.io/quote/v3 v3.1.0
+require github.com/go-sql-driver/mysql v1.9.3
 
-require (
-	golang.org/x/text v0.31.0 // indirect
-	rsc.io/sampler v1.3.1 // indirect
-)
+require filippo.io/edwards25519 v1.1.0 // indirect
